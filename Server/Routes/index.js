c5d6a604d46fed76b5a12c09da3636a2199edf57
Fahmid Ovi <fahmidovi9@gmail.com>
2022-07-15 00:23:55 -0400

    added first round ejs view

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -5,8 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
+const tournament_1 = __importDefault(require("../Models/tournament"));
 const index_1 = require("../Controllers/index");
 router.get('/', index_1.DisplayHomePage);
 router.get('/home', index_1.DisplayHomePage);
+router.get('/firstround', (req, res, next) => {
+    let id = req.params.id;
+    tournament_1.default.findById("62d0e534f8f41c97721beeb7", {}, {}, function (err, tournamentToView) {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.render('index', { title: 'First Round', page: 'tournament-firstround', tournament: tournamentToView });
+    });
+});
 exports.default = router;
 //# sourceMappingURL=index.js.map
