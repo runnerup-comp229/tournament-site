496686381932caaddf039ef2cf77968e0c4a0fbd
Fahmid Ovi <fahmidovi9@gmail.com>
2022-07-16 17:48:58 -0400

    Revised tournament model

--- Server/Models/tournament.js
+++ Server/Models/tournament.js
@@ -6,12 +6,37 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const Schema = mongoose_1.default.Schema;
 const TournamentSchema = new Schema({
-    TournamentName: String,
-    FirstRound: [String],
-    SecondRound: [String],
-    SemiFinal: [String],
-    RunnerUp: [String],
-    Final: [String]
+    Name: String,
+    Owner: String,
+    isActive: Boolean,
+    Participants: {
+        Team1: String,
+        Team2: String,
+        Team3: String,
+        Team4: String,
+        Team5: String,
+        Team6: String,
+        Team7: String,
+        Team8: String
+    },
+    SemiFinal: {
+        Team1: String,
+        Team2: String,
+        Team3: String,
+        Team4: String
+    },
+    RunnerUp: {
+        Team1: String,
+        Team2: String
+    },
+    Final: {
+        Team1: String,
+        Team2: String
+    },
+    First: String,
+    Second: String,
+    Third: String,
+    Fourth: String
 }, {
     collection: "tournaments"
 });
